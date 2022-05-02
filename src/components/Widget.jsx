2eee6340c562668f1bd35817ca5d9a95cdd33b27
John Marley da Silva Santos <65204093+JohnMarleySS@users.noreply.github.com>
2022-05-02 19:27:05 -0300

    Add files via upload

---
+++ src/components/Widget.jsx
@@ -0,0 +1,9 @@
+import { ChatTeardropDots } from "phosphor-react" 
+
+export function Widget(){
+    return (
+        <button className="bg-cyan-700">
+            <ChatTeardropDots size="200" className="w-6 h-6" />
+        </button>
+    )
+}
